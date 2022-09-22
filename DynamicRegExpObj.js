1732624bbddc0796ea9dcb0caa108034bd8b3976
Your Name <you@example.com>
2022-09-22 10:30:30 -0400

    fucntions

---
+++ DynamicRegExpObj.js
@@ -0,0 +1,22 @@
+/**
+ * if we don't know exactly the patterns we need to match against when
+ * writing a code, say we want to add uderscores around the name from a
+ * function but we don't know the name until its start running so we can't
+ * use the slash-based notation
+ * The only way to solve this is to buld up a string and use
+ * RegExp constructor on that.
+ */
+
+let name = "Henry";
+let text = "Henry is a suspicious character";
+let regexp = new RegExp("\\b(" + name + ")\\b", "gi");
+console.log(text.replace(regexp, "_$1_"));
+
+let text2 = "Henry is a suspicious character";
+let name2 = text2.split(" ")[0];
+let rg = new RegExp("\\b(" + name2 + ")\\b", "gi");
+console.log(text2.replace(rg, "_$1_")); //_Henry_ is a suspicious character
+
+let susp = text2.split(" ")[3];
+let sp = new RegExp("\\b(" + susp + ")\\b", "gi");
+console.log(text2.replace(sp, (s) => s.toUpperCase())); //Henry is a SUSPICIOUS character
