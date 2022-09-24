50c1e448415d19d8bd2ead13e2269dd234c07eb7
Your Name <you@example.com>
2022-09-24 11:22:45 -0400

    looping over the mathes

---
+++ LoopingRegExp.js
@@ -0,0 +1,20 @@
+let input = "A string with 3 numbers in it... 42,57 and 88.";
+let digits = /\b\d+\b/g;
+
+console.log(input.match(digit)); //['3', '3', '4', '5', '4']
+let d = digit.exec(input);
+console.log(d);
+//['3', index: 14, input: 'A string with 3 numbers in it ... 34 and 54', groups: undefined]
+console.log("value", d[0]);
+console.log("index", d.index);
+
+//let loop over the digit in the input string
+
+//create a binding
+let match;
+
+//use while loop inside which we goint to assign the
+//expression where  digit executes the input)
+while ((match = digits.exec(input))) {
+  console.log(`found number ${match[0]} at index ${match.index} `);
+}
