799a215837fa74f1ff237f13c86d54d6af3e6701
Your Name <you@example.com>
2022-09-24 10:29:59 -0400

    added lastindex

--- LastIndex.js
+++ LastIndex.js
@@ -18,3 +18,20 @@ console.log(match.index); //4
 // the exec call updates the lastIndex which was 3 to the index
 //after 4 which is 5. Now lastIndex is pointing at position 5
 console.log("nd call", pattern.lastIndex); //->5
+
+//NOTE STICKY OPTION enabled, the match will succeed only if
+// it start directly at the lastIndex position contrary to global
+// which will search ahead for the position where the match can start
+let gb = /abc/g;
+//console.log(gb.exec("yanx abc"))
+console.log((gb.lastIndex = 3)); //3
+let ind = gb.exec("yanx abc");
+console.log(ind.index); // 5
+console.log(gb.lastIndex); //8
+
+let sticky = /abc/y;
+console.log((sticky.lastIndex = 1)); //1
+let ind = sticky.exec("yanx abc");
+console.log(ind); //null
+// bcz it doesn't updates the lastindex position
+console.log(sticky.lastIndex); //0
