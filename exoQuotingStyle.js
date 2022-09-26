a9ddfebdfa0e697dbba125ba2f70069a62d4e6e7
Your Name <you@example.com>
2022-09-26 12:10:41 -0400

    add

---
+++ exoQuotingStyle.js
@@ -0,0 +1,10 @@
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/C/g, "D"));
+// → "I'm the cook," he said, "it's my job."
+
+// replacing single   quote to double quote while keeping the apostroth
+//untouched
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
+let doubeQuote = /(^|\W)'|'(\W|$)/g;
+console.log(text.replace(doubeQuote, '$1"$2'));
